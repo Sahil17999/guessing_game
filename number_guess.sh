2646f1ec97c7b21731f8465e29b10d6d66cb7390
Sahil Butt <flamingxv@gmail.com>
2024-07-06 17:13:04 +0000

    feat: add the two welcome prompts

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-cho $(($RANDOM % 1000 + 1))
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "Enter your username:"
+read INPUT
+
+#Retrieve user
+USER_EXIST=$($PSQL "SELECT name FROM players WHERE name='$INPUT'")
+
+if [[ -z $USER_EXIST ]]
+then
+  #Insert user if user doesn't exist in the table
+  INSERT_NAME=$($PSQL "INSERT INTO players(name) VALUES('$INPUT')")
+  echo -e "\nWelcome, $INPUT! It looks like this is your first time here."
+else
+  #print details about the user if user exists in the table
+  TOTAL_GAMES=$($PSQL "SELECT COUNT(*) FROM games JOIN players USING(player_id) WHERE name='$INPUT'")
+  BEST_GAME=$($PSQL "SELECT MIN(num_guesses) FROM games JOIN players USING(player_id) WHERE NAME='$INPUT'")
+  echo -e "\nWelcome back, $INPUT! You have played $TOTAL_GAMES games, and your best game took $BEST_GAME guesses."
+fi
+
+
+
+
+
